65165eb7c597f2a2616e29703b3c9dc8ee078ff5
MarManja <149817195+MarManja@users.noreply.github.com>
2026-05-21 19:18:17 -0600

    Commit 5: Actualización del archivo de lanzamiento de sensores
    Se agregaron argumentos configurables de lanzamiento para la cámara, el LiDAR y Teensy. Y se actualizaron las definiciones de nodos para incluir condiciones basadas en los argumentos.

--- src/neuracar_bringup/launch/sensors.launch.py
+++ src/neuracar_bringup/launch/sensors.launch.py
@@ -1,39 +1,68 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
-#ros2 launch realsense2_camera rs_launch.py
-#ros2 launch rplidar_ros rplidar_a3_launch.py 
+#  ros2 launch realsense2_camera rs_launch.py - Opción para lanzar solo la cámara
+#  ros2 launch rplidar_ros rplidar_a3_launch.py - Opción para lanzar solo el LiDAR
+
+# Opciones directamente de este launch:
+#  ros2 launch neuracar_bringup sensors.launch.py teensy:=false - sin Teensy
+#  ros2 launch neuracar_bringup sensors.launch.py camera:=false - sin cámara
+#  ros2 launch neuracar_bringup sensors.launch.py lidar:=false - sin LiDAR
 
 def generate_launch_description():
 
+    arg_camera = DeclareLaunchArgument(
+        'camera', default_value='true',
+        description='Lanzar la cámara RealSense D415'
+    )
+    arg_lidar = DeclareLaunchArgument(
+        'lidar', default_value='true',
+        description='Lanzar el LiDAR RPLidar A2M12'
+    )
+    arg_teensy = DeclareLaunchArgument(
+        'teensy', default_value='true',
+        description='Lanzar el puente serial con la Teensy'
+    )
+
+    
     realsense_launch = os.path.join(
         get_package_share_directory('realsense2_camera'),
         'launch',
         'rs_launch.py'
     )
 
-    camera = IncludeLaunchDescription(
+    camera_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(realsense_launch),
         launch_arguments={
-            # 'serial_no': '211222065514',
             'camera_name': 'camera',
             'camera_namespace': 'camera',
             'enable_color': 'true',
             'enable_depth': 'true',
             'enable_infra1': 'false',
             'enable_infra2': 'false',
+            # reducción de resolución para ahorrar CPU en la Jetson:
+            'color_width':      '640',
+            'color_height':     '480',
+            'color_fps':        '30',
+            'depth_width':      '640',
+            'depth_height':     '480',
+            'depth_fps':        '30',
         }.items()
+        condition=IfCondition(LaunchConfiguration('camera')),
     )
 
-    lidar = Node(
+    lidar_node = Node(
         package='rplidar_ros',
         executable='rplidar_node',
         name='rplidar_node',
         output='screen',
+        condition=IfCondition(LaunchConfiguration('lidar')),
         parameters=[{
             'channel_type': 'serial',
             'serial_port': '/dev/lidar',
@@ -45,19 +74,25 @@ def generate_launch_description():
         }]
     )
 
-    # teensy = Node(
-    # package='neuracar_sensors',
-    # executable='teensy_bridge_node',
-    # name='teensy_bridge',
-    # output='screen',
-    # parameters=[{
-    #     'port': '/dev/teensy',
-    #     'baudrate': 115200
-    # }]
-    # )
+    teensy_node = Node(
+        package='neuracar_sensors',
+        executable='teensy_bridge_node',
+        name='teensy_bridge',
+        output='screen',
+        condition=IfCondition(LaunchConfiguration('teensy')),
+        parameters=[{
+            'port':         '/dev/teensy',
+            'baudrate':     921600,
+            'wheel_radius': 0.033, # metros 
+            'gear_ratio':   9.5, # ratio encoder-motor → rueda
+        }]
+    )
 
     return LaunchDescription([
-        camera,
-        lidar,
-        # teensy,
+        arg_camera,
+        arg_lidar,
+        arg_teensy,
+        camera_node,
+        lidar_node,
+        teensy_node,
     ])
